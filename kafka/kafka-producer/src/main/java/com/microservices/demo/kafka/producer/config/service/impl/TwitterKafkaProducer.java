02496ab4f8e6e9bc4a7cc02590dc00af1b7847b5
puhung <twenet1212@gmail.com>
2023-06-08 16:36:32 -0700

    fix @Service annotaion missing error

--- kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/service/impl/TwitterKafkaProducer.java
+++ kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/service/impl/TwitterKafkaProducer.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import javax.annotation.PreDestroy;
 
+@Service
 public class TwitterKafkaProducer implements KafkaProducer<Long, TwitterAvroModel> {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaProducer.class);
 
